35641a495e86a350fe0b157bb7aaebf6d8aa44ab
mingkaic <mingkaichen2009@gmail.com>
2015-05-31 09:53:34 -0700

    Initial Commit

---
+++ TemplateTracking/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TemplateTracking
+//
+//  Created by mingkai on 2015-05-31.
+//  Copyright (c) 2015 mingkai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TemplateTracking/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TemplateTracking
+//
+//  Created by mingkai on 2015-05-31.
+//  Copyright (c) 2015 mingkai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
